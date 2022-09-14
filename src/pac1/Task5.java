40dc6becf30ebdcd65df092965b038e18cb6bcb4
BarAl35 <adam@cybertekschool.com>
2022-09-14 16:38:00 -0400

    Task6 is added

--- src/pac1/Task5.java
+++ src/pac1/Task5.java
@@ -1,4 +1,10 @@
 package pac1;
 
 public class Task5 {
+    /*
+    red means: it is not tracked in order to track use "Add" (in working directory)
+    Green means: it is tracked (it is in Staging Area)
+    White means: it is commited (it is in Local Repository)
+
+     */
 }
